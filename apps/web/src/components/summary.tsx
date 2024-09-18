80abe75419bbfdd481ac47a852e7e541a80b4cee
Jairo Koning <jairo.koning@gmail.com>
2024-09-18 16:11:20 -0300

    feat(frontend): add component summary

---
+++ apps/web/src/components/summary.tsx
@@ -0,0 +1,82 @@
+import { CheckCircle2, Plus } from 'lucide-react'
+import { GoalTrackerIcon } from './goal-tracker-icon'
+import { Button } from './ui/button'
+import { DialogTrigger } from './ui/dialog'
+import { Progress, ProgressIndicator } from './ui/progress-bar'
+import { Separator } from './ui/separator'
+import { OutlineButton } from './ui/outline-button'
+
+export function Summary() {
+  return (
+    <div className="py-10 max-w-[480px] px-5 mx-auto flex flex-col gap-6">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-3">
+          <GoalTrackerIcon />
+          <span>6 a 11 de Outubro</span>
+        </div>
+
+        <DialogTrigger asChild>
+          <Button size="sm">
+            <Plus className="size-4" />
+            Cadastrar meta
+          </Button>
+        </DialogTrigger>
+      </div>
+
+      <div className="flex flex-col gap-3">
+        <Progress value={8} max={15}>
+          <ProgressIndicator style={{ width: '53%' }} />
+        </Progress>
+
+        <div className="flex items-center justify-between text-xs text-zinc-400">
+          <span>
+            Você completou <span className="text-zinc-100">8</span> de{' '}
+            <span className="text-zinc-100">15</span> metas nessa semana.
+          </span>
+          <span>53%</span>
+        </div>
+      </div>
+
+      <Separator />
+
+      <div className="flex flex-wrap gap-3">
+        <OutlineButton>
+          <Plus className="size-4 text-zinc-600" />
+          Acordar cedo
+        </OutlineButton>
+        <OutlineButton>
+          <Plus className="size-4 text-zinc-600" />
+          Meditar
+        </OutlineButton>
+        <OutlineButton>
+          <Plus className="size-4 text-zinc-600" />
+          Estudar
+        </OutlineButton>
+        <OutlineButton>
+          <Plus className="size-4 text-zinc-600" />
+          Fazer exercícios
+        </OutlineButton>
+      </div>
+
+      <div className="flex flex-col gap-6">
+        <h2 className="text-3xl font-medium">Sua semana</h2>
+        <div className="flex flex-col gap-4">
+          <h3 className="font-medium">
+            Domingo{' '}
+            <span className="text-zinc-400 text-xs">(6 de Outubro)</span>
+          </h3>
+          <ul className="flex flex-col gap-3">
+            <li className="flex items-center gap-2">
+              <CheckCircle2 className="size-4 text-pink-500" />
+              <span className="text-sm text-zinc-400">
+                Você completou "
+                <span className="text-zinc-100">Acordar cedo</span>" às{' '}
+                <span className="text-zinc-100">05:00</span>
+              </span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
