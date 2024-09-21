1b363a8bc1e6df8b5873c6624fe062708c0ccaea
Jairo Koning <jairo.koning@gmail.com>
2024-09-21 05:57:47 -0300

    refact(backend): change pending goals route

--- apps/backend/src/http/routes/get-pending-goals.ts
+++ apps/backend/src/http/routes/get-pending-goals.ts
@@ -1,10 +1,10 @@
-import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
-import GetWeekPendingGoals from "../../queries/get-week-pending-goals";
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import GetWeekPendingGoals from '../../queries/get-week-pending-goals'
 
-export const getPendingGoalsRoute: FastifyPluginAsyncZod = async (app) => {
-	app.get("/pending-goals", async () => {
-		const getWeekPendingGoals = new GetWeekPendingGoals();
-		const pendingGoals = await getWeekPendingGoals.execute();
-		return pendingGoals;
-	});
-};
+export const getPendingGoalsRoute: FastifyPluginAsyncZod = async app => {
+  app.get('/goals/pending', async () => {
+    const getWeekPendingGoals = new GetWeekPendingGoals()
+    const pendingGoals = await getWeekPendingGoals.execute()
+    return pendingGoals
+  })
+}
